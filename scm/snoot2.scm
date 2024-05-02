fc9488e7e032102af0956931e6384a049a094119
Mikael Brockman <mikael@brockman.se>
2024-05-02 22:56:48 +0300

    wip

--- scm/snoot2.scm
+++ scm/snoot2.scm
@@ -1,3 +1,21 @@
+;; What are the axioms of this thing?
+;;
+;; We want to implement structured concurrency,
+;; in a way that can interoperate with promises.
+;;
+;; That just means there's a hierarchy of task scopes,
+;; and we can't leave a task scope until all the tasks
+;; in it are done.
+;;
+;; When a task scope returns, it must somehow join all
+;; its subtasks.  Java has the StructuredTaskScope<T> class
+;; for example, with a try (var scope = new StructuredTaskScope())
+;; block that demands .join() be called on it before the block
+;; can exit.  I believe it's known as a nursery in other systems
+;; like Python's trio.
+;;
+;; We use a prompt to implement this.
+
 (use-modules (ice-9 match))
 
 (define task-tag (make-prompt-tag 'task))
@@ -17,7 +35,13 @@
 		 (abort-to-prompt task-tag 'display)
 		 (display id)
 		 (display " ")
-		 (nest id k))))))
+		 (nest id k)))
+	  ((k 'suspend)
+	   ;; this is really the core of the thing...
+	   ;; we're going to save the continuation...
+	   ;; and then call it later...
+	   ;; tricky business...
+		   ))))
 
 (define (root continuation)
   (call-with-prompt task-tag 
