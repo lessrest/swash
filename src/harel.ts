338802b815dc168902ddf3ee51bffca7bf54eeb5
Mikael Brockman <mikael@brockman.se>
2024-05-11 01:21:11 +0200

    harel pretty

--- src/harel.ts
+++ src/harel.ts
@@ -1,123 +1,117 @@
 interface Sync<T> {
-  post?: T[]
-  wait?: T[]
-  stop?: T[]
+  have?: T[]
+  want?: T[]
+  deny?: T[]
 }
 
-interface Thread<T> {
+interface Task<T> {
   name: string
-  cont: Generator<Sync<T>>
   sync: Sync<T>
+  proc: Generator<Sync<T>>
 }
 
-class Multimap<K, V> extends Map<K, V[]> {
+class Multimap<K, V> extends Map<K, Set<V>> {
   add(key: K, value: V) {
-    this.set(key, [...this.get(key), value])
+    this.get(key).add(value)
   }
 
-  get(key: K): V[] {
-    return super.get(key) ?? []
+  get(key: K): Set<V> {
+    this.set(key, super.get(key) ?? new Set())
+    return super.get(key)!
   }
 }
 
-export function step<Post>(threads: Set<Thread<Post>>) {
-  const posters = new Multimap<Post, Thread<Post>>()
-  const waiters = new Multimap<Post, Thread<Post>>()
-  const stopped = new Set<Post>()
-  const exiters = new Set<Thread<Post>>()
-
-  for (const thread of threads) {
-    const { post, wait, stop } = thread.sync
-    for (const x of post ?? []) posters.add(x, thread)
-    for (const x of wait ?? []) waiters.add(x, thread)
-    for (const x of stop ?? []) stopped.add(x)
-  }
-
-  const chosen = [...posters.keys()].find((x) => !stopped.has(x))
-
-  if (chosen) {
-    console.log("✱", chosen)
-    for (const thread of new Set([
-      ...posters.get(chosen),
-      ...waiters.get(chosen),
-    ])) {
-      const { done, value } = thread.cont.next()
-      if (done) {
-        exiters.add(thread)
-      } else {
-        thread.sync = value
-      }
-    }
-
-    return threads.difference(exiters)
-  }
-
-  return null
-}
-
-export function* exec<T>(
-  threads: Set<Thread<T>>,
-): Generator<{ state: string; threads: Set<Thread<T>> }> {
+export function* work<Item>(
+  team: Set<Task<Item>>,
+): Generator<Set<Task<Item>>, Set<Task<Item>>> {
   for (;;) {
-    const result = step(threads)
+    const have = new Multimap<Item, Task<Item>>()
+    const want = new Multimap<Item, Task<Item>>()
+    const deny = new Multimap<Item, Task<Item>>()
 
-    if (result === null) {
-      yield { state: "blocked", threads }
+    for (const task of team) {
+      for (const x of task.sync.have ?? []) have.add(x, task)
+      for (const x of task.sync.want ?? []) want.add(x, task)
+      for (const x of task.sync.deny ?? []) deny.add(x, task)
+    }
+
+    const item = [...have.keys()].find((x) => !deny.has(x))
+
+    if (!item) {
+      return team
     } else {
-      threads = result
+      console.log("✱", item)
+
+      for (const task of have.get(item).union(want.get(item))) {
+        const { done, value } = task.proc.next()
+        if (done) {
+          team.delete(task)
+        } else {
+          task.sync = value
+        }
+      }
+
+      yield team
     }
   }
 }
 
-export function run<T>(threads: Iterable<Thread<T>>) {
-  const { done, value } = exec(new Set(threads)).next()
-  if (!done) {
-    console.log("◼︎", value.state)
+export function exec<T>(tasks: Iterable<Task<T>>) {
+  let x = new Set(tasks)
+  for (;;) {
+    const { done, value } = work(x).next()
+    if (done) {
+      console.log("◼︎", value)
+      return
+    } else {
+      console.log("✅", value)
+      x = value
+    }
   }
 }
 
-function noop<T>(name: string): Thread<T> {
+function noop<T>(name: string): Task<T> {
   return {
     name,
-    cont: (function* () {})(),
+    proc: (function* () {})(),
     sync: {},
   }
 }
 
-export function start<T>(
+export function task<T>(
   name: string,
-  thunk: () => Generator<Sync<T>>,
-): Thread<T> {
-  const cont = thunk()
-  const { done, value: sync } = cont.next()
-  return done ? noop(name) : { name, cont, sync }
+  init: () => Generator<Sync<T>>,
+): Task<T> {
+  const proc = init()
+  const { done, value: sync } = proc.next()
+  return done ? noop(name) : { name, proc, sync }
 }
 
 function demo() {
-  run([
-    start("fill hot", function* () {
+  exec([
+    task("fill hot", function* () {
       for (;;) {
-        yield { wait: ["water-low"] }
-        yield { post: ["add-hot"] }
-        yield { post: ["add-hot"] }
-        yield { post: ["add-hot"] }
+        yield { want: ["water-low"] }
+        yield { have: ["add-hot"] }
+        yield { have: ["add-hot"] }
+        yield { have: ["add-hot"] }
       }
     }),
-    start("initially low", function* () {
-      yield { post: ["water-low"] }
+    task("initially low", function* () {
+      yield { have: ["water-low"] }
     }),
-    start("fill cold", function* () {
+    task("fill cold", function* () {
       for (;;) {
-        yield { wait: ["water-low"] }
-        yield { post: ["add-cold"] }
-        yield { post: ["add-cold"] }
-        yield { post: ["add-cold"] }
+        yield { want: ["water-low"] }
+        yield { have: ["add-cold"] }
+        yield { have: ["add-cold"] }
+        yield { have: ["add-cold"] }
       }
     }),
-    start("balance hot/cold", function* () {
+    task("balance hot/cold", function* () {
       for (;;) {
-        yield { wait: ["add-hot"], stop: ["add-cold"] }
-        yield { wait: ["add-cold"], stop: ["add-hot"] }
+        yield { want: ["add-hot"], deny: ["add-cold"] }
+        yield { want: ["add-cold"], deny: ["add-hot"] }
       }
     }),
   ])
