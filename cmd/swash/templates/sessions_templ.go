e8a741eb94251884856df2af0b04fd4041b52d51
Mikael Brockman <mikael@brockman.se>
2025-12-11 22:30:25 +0200

    add lefthook pre-commit hooks for fmt, vet, staticcheck, tests

--- cmd/swash/templates/sessions_templ.go
+++ cmd/swash/templates/sessions_templ.go
@@ -5,12 +5,12 @@ package templates
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
 	"fmt"
 	"time"
-
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
 
 	swrt "github.com/mbrock/swash/internal/runtime"
 )
